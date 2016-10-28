b5814f7e85f86611d8e4ff0f13a4f9f97bfb9621
YaSergo <medintsev@yandex-team.ru>
2016-10-28 18:39:28 +0300

    Первый коммит

---
+++ main.R
@@ -0,0 +1,20 @@
+# загрузка данных
+cpm_data <- read.csv("~/Downloads/query_result (33).csv")
+cpm_data <- cpm_data[cpm_data$avg_cpm > 0, ]
+
+# функция для расчёта p-value
+pvalue_tdist <- function(m, s, n, threshold = 10){
+  # m - mean
+  # s - sd
+  # n - n
+  xbar <- m * (1 + threshold / 100)
+  t <- (xbar - m) / (s / sqrt(n))
+  result <- 2*pt(-abs(t), df=n-1)
+  return(result)
+}
+
+# считаем p-value
+cpm_data$pvalue <- pvalue_tdist(m = cpm_data$avg_cpm, s = cpm_data$sd, n = cpm_data$n)
+
+# формируем data frame с "хорошими" hyper_id
+cpm_data_good <- cpm_data[cpm_data$pvalue < 0.05, ]
