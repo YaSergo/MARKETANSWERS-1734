c4317c4ea7efeff46517fb4a31cb1371c2bef065
YaSergo <medintsev@yandex-team.ru>
2016-11-01 10:46:27 +0300

    Функция плотности вероятности
    В коде R реализовал построение функции плотности вероятности для
    наиболее частовстречающихся категорий;
    Оптимизировал SQL запрос к HIVE и добавил вывод новых полей

--- SQL/sql_query_cpm_simple.sql
+++ SQL/sql_query_cpm_simple.sql
@@ -1,19 +1,22 @@
-SELECT hyper_id, avg(cpm) as avg_cpm, stddev_samp(cpm) as sd, count(*) as n
+SELECT
+  hyper_id, name, category_id,
+  avg(cpm) as avg_cpm,
+  stddev_samp(cpm) as sd,
+  count(*) as n,
+  collect_list(cpm) AS cpm_array
 FROM
 (
-  SELECT main_data.* -- оставляем в данных только те hyper_id, которые были представлены все 21 дня
-  FROM market_page_cpm_simple as main_data RIGHT JOIN
-  (
-    SELECT hyper_id
-    FROM
-    (
-      SELECT hyper_id, count(*) as num
-      FROM market_page_cpm_simple
-      GROUP BY hyper_id
-    ) t
-    WHERE num = 21
-  ) good_hyper_id
-  ON main_data.hyper_id = good_hyper_id.hyper_id
+  SELECT
+    main_data.*,
+    details.name, details.category_id
+  FROM medintsev.market_page_cpm_simple as main_data
+  LEFT JOIN dictionaries.models AS details
+  ON main_data.hyper_id = details.id
 ) a
-GROUP BY hyper_id
-HAVING avg_cpm > 0
+GROUP BY hyper_id, name, category_id
+HAVING
+  avg_cpm > 0 AND
+  n = 21 -- берём только hyper_id, которые были представлены 21 день
+  -- если это условие не использоть, то среднее значение и среднеквадратичное
+  -- отклонение будут рассчитаны не верно. Ряд тогда будет таким:
+  -- 6, 2, 3 вместо 0, 0, ..., 0, 0, 6, 2, 3
--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 # загрузка данных
-cpm_data <- read.csv("~/Downloads/query_result (33).csv")
+cpm_data <- read.csv("~/Downloads/query_result (35).csv")
 cpm_data <- cpm_data[cpm_data$avg_cpm > 0, ]
 
 # функция для расчёта p-value
@@ -15,8 +15,22 @@ pvalue_tdist <- function(m, s, n, threshold = 10){
 
 # считаем p-value
 cpm_data$pvalue <- pvalue_tdist(m = cpm_data$avg_cpm, s = cpm_data$sd, n = cpm_data$n)
+write.csv(x = cpm_data, file = "output/cpm_data.csv")
 
 # формируем data frame с "хорошими" hyper_id
 cpm_data_good <- cpm_data[cpm_data$pvalue < 0.05, ]
 
-write.csv(x = cpm_data_good, file = "output/good_hyper_id.csv")
+write.csv(x = cpm_data_good, file = "output/cpm_data_good.csv")
+
+# Графики плотности вероятности для наиболее частовстречающихся категорий
+library(ggplot2)
+category_id_good <- aggregate(name ~ category_id, data = cpm_data_good, FUN = length)
+category_id_good <-
+  category_id_good$category_id[category_id_good$name > quantile(category_id_good$name, 0.85)]
+category_id_good <- as.numeric(as.character(category_id_good))
+
+cpm_data_good$category_id <- as.factor(cpm_data_good$category_id)
+
+p <- ggplot(data = cpm_data_good[cpm_data_good$category_id %in% category_id_good, ], aes(x = avg_cpm, fill = category_id))+
+  geom_density(alpha = 0.6)
+ggsave(filename="./output/density.jpg", plot=p)
