98a66ac3fd6087d5c939f02daaad117fa61d08cf
YaSergo <medintsev@yandex-team.ru>
2016-11-01 15:40:27 +0300

    Построил графики корреляции

--- main.R
+++ main.R
@@ -34,3 +34,46 @@ cpm_data_good$category_id <- as.factor(cpm_data_good$category_id)
 p <- ggplot(data = cpm_data_good[cpm_data_good$category_id %in% category_id_good, ], aes(x = avg_cpm, fill = category_id))+
   geom_density(alpha = 0.6)
 ggsave(filename="./output/density.jpg", plot=p)
+
+offers_data <- read.csv(file = "~/Downloads/query_result (39).csv", na.strings = "NULL")
+#cpm_data_ext <- merge(x = cpm_data, y = offers_data, by = "hyper_id", all.x = TRUE)
+cpm_data_good_ext <- merge(x = cpm_data_good, y = offers_data, by = "hyper_id", all.x = TRUE)
+cpm_data_good_ext <- cpm_data_good_ext[complete.cases(cpm_data_good_ext), ]
+
+corr_func <- function(coef, cpm, cpc, feeincome){
+  return(cor(x = cpm, y = cpc * coef + feeincome))
+}
+
+coef <- seq(from = 0, to = 75, by = 0.1)
+y_all <- sapply(coef, function(x){corr_func(x, cpm = cpm_data_good_ext$avg_cpm,
+                                   cpc = cpm_data_good_ext$avg_cbid_all,
+                                   feeincome = cpm_data_good_ext$avg_feeincome_all)})
+y_15 <- sapply(coef, function(x){corr_func(x, cpm = cpm_data_good_ext$avg_cpm,
+                                           cpc = cpm_data_good_ext$avg_cbid_15,
+                                           feeincome = cpm_data_good_ext$avg_feeincome_15)})
+y_30 <- sapply(coef, function(x){corr_func(x, cpm = cpm_data_good_ext$avg_cpm,
+                                           cpc = cpm_data_good_ext$avg_cbid_30,
+                                           feeincome = cpm_data_good_ext$avg_feeincome_30)})
+y_45 <- sapply(coef, function(x){corr_func(x, cpm = cpm_data_good_ext$avg_cpm,
+                                           cpc = cpm_data_good_ext$avg_cbid_45,
+                                           feeincome = cpm_data_good_ext$avg_feeincome_45)})
+
+
+plotdata <- data.frame(coef = coef, y = c(y_all, y_15, y_30, y_45),
+                       type = as.factor(c(rep("all", length(y_all)),
+                                          rep("15", length(y_15)),
+                                          rep("30", length(y_30)),
+                                          rep("45", length(y_45)))
+                       )
+)
+p <- ggplot(data = plotdata, aes(x = coef, y = y, col = type))+
+  geom_line() +
+  ylim(0.6, 0.8) +
+  ylab("Корреляция")
+print(p)
+ggsave(filename="./output/corr.jpg", plot=p)
+
+cpm_data_good_ext$cpm.predict <- cpm_data_good_ext$avg_cbid_se*28 + cpm_data_good_ext$avg_feeincome_fe
+ggplot(data = cpm_data_good_ext, aes(x = cpm_data_good_ext$cpm.predict,
+                                     y = cpm_data_good_ext$avg_cpm))+
+  geom_point(alpha = 0.8)
